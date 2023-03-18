23aad4a89077725fd28e119de71fde2a013d856c
Diam Diallo <70403651+Diam-98@users.noreply.github.com>
2023-03-18 02:00:34 +0000

    menu setup

--- assets/app.js
+++ assets/app.js
@@ -12,4 +12,30 @@ import './styles/app.css';
 import './bootstrap';
 import 'flowbite';
 
+let mobile_menu = document.querySelector('.mobile_menu');
+let mobile_menu_btn = document.querySelector('#mobile-menu-btn');
+let mobile_menu_close = document.querySelector('#close-mobile-menu-btn');
+
+console.log(mobile_menu.className)
+
+mobile_menu.innerHTML = document.querySelector('.nav-menu').innerHTML;
+
+mobile_menu_btn.onclick = function (){
+    mobile_menu.classList.replace( 'hide-menu', 'mobile_menu');
+    mobile_menu_btn.classList.remove('active-btn');
+    mobile_menu_btn.style.display = "none";
+    mobile_menu_close.style.display = "block";
+    mobile_menu_close.classList.add('active-btn');
+}
+
+mobile_menu_close.onclick = function(){
+    mobile_menu.classList.replace( 'mobile_menu','hide-menu');
+    mobile_menu_close.classList.remove('active-btn');
+    mobile_menu_close.style.display = "none";
+    mobile_menu_btn.style.display = "block";
+    mobile_menu_btn.classList.add('active-btn');
+}
+
+
+
 
